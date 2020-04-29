cacb33b4bbb5d86b9f3b1e111739e73fafe354e9
LeonNguyen <n.anhtuan0808@gmail.com>
2020-04-29 18:47:24 +0700

    feat: update code

--- src/views/pages/Index.vue
+++ src/views/pages/Index.vue
@@ -15,57 +15,12 @@
 					<!-- Messages Dropdown Menu -->
 					<li class="nav-item dropdown">
 						<a class="nav-link" data-toggle="dropdown" href="#">
-							<i class="pi pi-comments"></i>
-							<span class="badge badge-danger navbar-badge">3</span>
+							<i class="pi pi-user"></i>
 						</a>
 
 						<div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-							<a href="#" class="dropdown-item">
-								<!-- Message Start -->
-								<div class="media">
-									<div class="media-body">
-										<h3 class="dropdown-item-title">
-											Brad Diesel
-											<span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
-										</h3>
-										<p class="text-sm">Call me whenever you can...</p>
-										<p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-									</div>
-								</div>
-								<!-- Message End -->
-							</a>
 							<div class="dropdown-divider"></div>
-							<a href="#" class="dropdown-item">
-								<!-- Message Start -->
-								<div class="media">
-									<div class="media-body">
-										<h3 class="dropdown-item-title">
-											John Pierce
-											<span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-										</h3>
-										<p class="text-sm">I got your message bro</p>
-										<p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-									</div>
-								</div>
-								<!-- Message End -->
-							</a>
-							<div class="dropdown-divider"></div>
-							<a href="#" class="dropdown-item">
-								<!-- Message Start -->
-								<div class="media">
-									<div class="media-body">
-										<h3 class="dropdown-item-title">
-											Nora Silvester
-											<span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
-										</h3>
-										<p class="text-sm">The subject goes here</p>
-										<p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-									</div>
-								</div>
-								<!-- Message End -->
-							</a>
-							<div class="dropdown-divider"></div>
-							<a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+							<a href="#" class="dropdown-item dropdown-footer">Sign out</a>
 						</div>
 					</li>
 
